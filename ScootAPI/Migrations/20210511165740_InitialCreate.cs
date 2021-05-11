f6a1a1c46da28bf4f653e674da672b8d284b0277
MaxenceCuguen <cuguen.pro@gmail.com>
2021-05-11 19:10:52 +0200

    add migrations

---
+++ ScootAPI/Migrations/20210511165740_InitialCreate.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using ScootAPI.Models;
+
+namespace ScootAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "scooter",
+                columns: table => new
+                {
+                    idScooter = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    location = table.Column<Customer>(type: "jsonb", nullable: false),
+                    isDisabled = table.Column<bool>(type: "boolean", nullable: false),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    idZone = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("scooter_pkey", x => x.idScooter);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "zone",
+                columns: table => new
+                {
+                    idZone = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    isDisabled = table.Column<bool>(type: "boolean", nullable: false),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("zone_pkey", x => x.idZone);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "scooter");
+
+            migrationBuilder.DropTable(
+                name: "zone");
+        }
+    }
+}
